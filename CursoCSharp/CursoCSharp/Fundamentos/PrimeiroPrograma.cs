090f9cfbbf5b40ba9a055b9dfaef18bd3b8d7edc
Joana Santos <Joana_Santos004@outlook.com>
2022-12-26 21:22:43 -0300

    Salvando início do projeto do curso

---
+++ CursoCSharp/CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,15 @@
+﻿namespace CursoCSharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.Write("Primeiro "); //Escreve e mantém o cursor na msm linha
+            Console.WriteLine("programa terminou!"); //Escreve e joga o cursor pra próx linha
+            // CW + tab + tab = Console.WriteLine()
+            // Ctrl + k + d = Formatação
+
+            //Ultima aula: Cap 3, aula 20
+        }
+    }
+}
